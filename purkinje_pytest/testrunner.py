e18468595e4b8ad09b28470b36f8cdd584ae2554
Bernhard Biskup <bbiskup@gmx.de>
2014-12-25 20:23:58 +0100

    check max_user_watches on startup of TestRunner

--- purkinje_pytest/testrunner.py
+++ purkinje_pytest/testrunner.py
@@ -6,7 +6,8 @@ from __future__ import print_function
 from watchdog.observers import Observer
 import logging
 import time
-from .hander import Handler
+import os
+from .handler import Handler
 logger = logging.getLogger(__file__)
 
 
@@ -21,6 +22,9 @@ class TestRunner:
 
        TODO: ensure that ulimit is sufficient for number of
              files to be monitored
+       TODO: exclude subdirectories like .tox (configurable)
+       TODO: observer should only look at relevant files (_filter() == True).
+             Is this supported?
     """
 
     def __init__(self, dir):
@@ -29,11 +33,41 @@ class TestRunner:
         self.event_handler = Handler()
         self.observer = Observer()
 
+    @staticmethod
+    def check_watch_limit(dir):
+        max_user_watches = TestRunner.get_max_user_watches()
+        file_count = TestRunner.get_file_count(dir)
+        if file_count > max_user_watches:
+            raise Exception(('User limit "max_user_watches" too low'
+                             ' ({}) to watch all {} files')
+                            .format(max_user_watches, file_count))
+
+    @staticmethod
+    def get_file_count(dir):
+        """Counts how many relevant files there are to watch
+        """
+        result = 0
+        for roots, dirs, files in os.walk(dir):
+            for f in files:
+                result += 1
+
+    @staticmethod
+    def get_max_user_watches():
+        """Checks system limit max_user_watches to determine whether
+           it is possible to watch all relevant files
+        """
+        with open('/proc/sys/fs/inotify/max_user_watches') as f:
+            return int(f.read())
+
     def start(self):
         """Watch directory forever and execute test cases
         """
         print('{}: watching directory "{}"'.format(self.__class__, self._dir))
         self.observer.schedule(self.event_handler, self._dir, recursive=True)
+
+        # TODO should be checked when new files get added
+        self.check_watch_limit(self._dir)
+
         self.observer.start()
 
         try:
